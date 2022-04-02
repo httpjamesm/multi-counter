5980197ac0d5bdf88cdec001cda0e8168f04d623
httpjamesm <github@httpjames.space>
2022-04-01 21:06:05 -0400

    basic homepage with counter component

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import Counter from "./components/Counter.svelte";
+</script>
+
+<body>
+
+    <div class="container">
+        <h1>Simple Counter</h1>
+        <h2>Keep track of things efficiently.</h2>
+        <Counter />
+    </div>
+
+</body>
+
+<style>
+    :global(body) {
+        padding: 0;
+        margin: 0;
+    }
+
+    body {
+        background: #171717;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .container {
+        color: white;
+        text-align: center;
+    }
+
+    h2 {
+        font-weight: normal;
+    }
+
+</style>
---
+++ src/components/Counter.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    export let name = "Unnamed Counter";
+    export let description = "No description yet.";
+    export let color = "#ffffff";
+</script>
+
+<body>
+    <div class="container">
+        <div class="colorParent">
+            <div class="color" style={`background: ${color}`} />
+        </div>
+        <div class="info">
+            <p class="title">{name}</p>
+            <p class="description">
+                {description}
+            </p>
+        </div>
+    </div>
+</body>
+
+<style>
+    .container {
+        width: 20rem;
+        border-radius: 10px;
+        border: 1px solid white;
+        color: white;
+        height: 5rem;
+        padding: 1rem;
+        display: flex;
+        align-items: center;
+    }
+
+    .colorParent {
+        width: 40%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .color {
+        width: 5rem;
+        height: 5rem;
+        border-radius: 10px;
+    }
+
+    .info {
+        width: 100%;
+        padding-left: 1rem;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .title {
+        font-weight: bold;
+        font-size: 1.5rem;
+        text-align: left;
+        margin: 0;
+    }
+
+    .description {
+        margin: 0;
+        text-align: left;
+    }
+</style>
